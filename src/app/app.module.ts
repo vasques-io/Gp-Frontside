fe2d757cc8108d7a67ca099916c86869f6308865
Felipe Vasques <felipeovgh@gmail.com>
2023-09-19 16:19:08 -0400

    Inicialização da construção do componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponentComponent } from './components/sidebar-component/sidebar-component.component';
-import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponentComponent,
-    HomeComponentComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
